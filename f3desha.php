14a5368c9a2b8b740eb8acd6b143fb9587a0717d
Oleksandr Kuzmenco <okuzmenko@determine.com>
2018-05-02 11:11:52 +0300

    added manual in file

--- f3desha.php
+++ f3desha.php
@@ -1,9 +1,21 @@
 <?php
-	///////////////////////////////////////
-	///// Script for Determine apps routines optimization
-	///// Started on 30.04.2018
-	///// Author: Alexander Kuzmenko aka F3desha (skype: kas_illuzion)
-	//////////////////////////////////////
+///////////////////////////////////////
+///// Script for Determine apps routines optimization
+///// Started on 30.04.2018
+///// Author: Alexander Kuzmenko aka F3desha (skype: kas_illuzion)
+///
+///    MANUAL
+///    1. Clone this file to your vagrant root (the same directory where www folder *www/workspace/apps* )
+///    2. Go to the directory that contains f3desha.php with console
+///    3. Type:
+///       php f3desha.php git_checkout -branch=<branch_name>
+///
+///    Script will run through all apps in apps folder and change branches to the branch you selected
+///    in -branch option. If branch is already local it will just switch to it. If branch is only remote
+///    it will download it and switch. If you already on selected branch, nothing will happen
+///
+///    Good luck!
+//////////////////////////////////////
 
 	const ROOT_DIR = __DIR__;
 	const PATH_TO_APPS = 'www'.DIRECTORY_SEPARATOR.'workspace'.DIRECTORY_SEPARATOR.'apps';
