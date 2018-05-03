5b820262917a03ce1405d6557a78d3e2e454c200
Oleksandr Kuzmenco <okuzmenko@determine.com>
2018-05-03 15:13:18 +0300

    added possibility for checkouting single app

--- f3desha.php
+++ f3desha.php
@@ -19,7 +19,7 @@
 
 	const ROOT_DIR = __DIR__;
 	const PATH_TO_APPS = 'www'.DIRECTORY_SEPARATOR.'workspace'.DIRECTORY_SEPARATOR.'apps';
-	$excluded_apps = [];
+
 	$bootstrap = false;
 	$options = [];
 	$flags = [];
@@ -54,48 +54,50 @@
 
 	function git_checkout($options, $flags){
 
-		global $excluded_apps;
+		$excluded_apps = [];
+		$checkout_only = false;
+
+		array_key_exists('checkout_only', $options) ? $checkout_only = $options['checkout_only'] : null;
+		array_key_exists('exclude_apps', $options) ? $excluded_apps = explode(',',$options['exclude_apps']) : null;
 
 		$all_modules = directories_on_path(PATH_TO_APPS);
 
 		if($all_modules){
-			foreach ($all_modules as $i=>$module_directory){
-				if(!in_array($module_directory, $excluded_apps)){
-					chdir(ROOT_DIR);
-					chdir(PATH_TO_APPS);
-					chdir($module_directory);
-					//We are now in modules directory
-
-					//Get list of all branches
-					$branches_list = [];
-					exec('git branch', $i);
-					$dirty_branches_list = $i;
-
-					//Get active branch
-					$pristine_branches_list = [];
-					foreach ($dirty_branches_list as $branch){
-						if($branch[0] === '*' && $branch[1] === ' '){
-							$active_branch = ltrim($branch, '* ');
-							$pristine_branches_list[] = $active_branch;
-						} else {
-							$pristine_branches_list[] = trim($branch);
+			foreach ($all_modules as $i=>$module_directory) {
+				if (!in_array($module_directory, $excluded_apps)) {
+					if (!$checkout_only || $checkout_only === $module_directory) {
+						chdir(ROOT_DIR);
+						chdir(PATH_TO_APPS);
+						chdir($module_directory);
+						//We are now in modules directory
+						//Get list of all branches
+						$branches_list = [];
+						exec('git branch', $i);
+						$dirty_branches_list = $i;
+						//Get active branch
+						$pristine_branches_list = [];
+						foreach ($dirty_branches_list as $branch) {
+							if ($branch[0] === '*' && $branch[1] === ' ') {
+								$active_branch = ltrim($branch, '* ');
+								$pristine_branches_list[] = $active_branch;
+							} else {
+								$pristine_branches_list[] = trim($branch);
+							}
 						}
-					}
-
-					echo $module_directory.":\n";
-
-					//If branch option exists, check if it exists in local git
-					if(array_key_exists('branch', $options)){
-						if(in_array($options['branch'], $pristine_branches_list)){
-							//Option branch already exists. No need to create new branch
-							$checkout_command = 'git checkout '.$options['branch'];
-						} else {
-							//Create new branch. Get the name from branch
-							$checkout_command = 'git checkout -b '.$options['branch'].' origin/'.$options['branch'];
+						echo $module_directory . ":\n";
+						//If branch option exists, check if it exists in local git
+						if (array_key_exists('branch', $options)) {
+							if (in_array($options['branch'], $pristine_branches_list)) {
+								//Option branch already exists. No need to create new branch
+								$checkout_command = 'git checkout ' . $options['branch'];
+							} else {
+								//Create new branch. Get the name from branch
+								$checkout_command = 'git checkout -b ' . $options['branch'] . ' origin/' . $options['branch'];
+							}
+							exec($checkout_command);
+						} elseif (!array_key_exists('branch', $options)) {
+							echo 'Enter "-branch=$branch_name" option for function running properly' . "\n";
 						}
-						exec($checkout_command);
-					} elseif(!array_key_exists('branch', $options)){
-						echo 'Enter "-branch=$branch_name" option for function running properly'."\n";
 					}
 				}
 			}
